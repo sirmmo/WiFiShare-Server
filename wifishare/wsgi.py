67214b83db333941f09f1e704f9a31b96931872f
Marco Montanari <sirmmo@gmail.com>
2014-01-09 16:36:08 +0100

    init

---
+++ wifishare/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for wifishare project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wifishare.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
